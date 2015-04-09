e9c75aafd57435ef422bcade18b76e4022eb44ac
pharm <lukaszek.marek@gmail.com>
2015-04-09 11:10:58 +0000

    Changed title setup to more centralized

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | RoR"
+    assert_select "title", "RoR"
   end
 
   test "should get help" do
